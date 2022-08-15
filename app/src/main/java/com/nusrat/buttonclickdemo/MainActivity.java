e1c2cd7898ac17222a91ec1e9989edcf82ae0d09
Nusrat Jannat <nusratjannat25.1998@gmail.com>
2022-08-15 20:58:34 +0600

    ButtonClickDemo

---
+++ app/src/main/java/com/nusrat/buttonclickdemo/MainActivity.java
@@ -0,0 +1,19 @@
+package com.nusrat.buttonclickdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void gotoNewActivity(View view) {
+        Toast.makeText(MainActivity.this,"Button Click hoise!",Toast.LENGTH_SHORT).show();
+    }
+}
